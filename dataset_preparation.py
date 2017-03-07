7ae0e042f71a0464e439e97a0bbb2ef043b37a30
Guillaume <guillaume.guerdoux@hotmail.fr>
2017-03-07 22:30:41 +0100

    Save scrapped images directly in poster folder and not create intermediate folders

--- dataset_preparation.py
+++ dataset_preparation.py
@@ -10,30 +10,29 @@ def create_dataset(input_json_path, output_genres_json_path,
         movies = json.load(f)
 
     genres = set()
-    dataset = {}
+    labels = {}
     i = 0
     for movie in movies:
         try:
             i += 1
             print(i)
-            temp_title = movie['movie_title'].strip().lower()
+            image_title = movie['movie_title'].strip().lower() + '.jpg'
             temp_genres = movie['genres']
-            image_url = movie['image_urls'][0]
+            # image_url = movie['image_urls'][0]
             for genre in movie['genres']:
                 genres.add(genre)
             im = Image.open(requests.get(image_url, stream=True).raw)
-            os.mkdir(output_poster_directory + '/' + temp_title)
-            im.save(output_poster_directory + '/' + temp_title + '/' + temp_title + '.jpg')
-            dataset[temp_title] = temp_genres
+            im.save(output_poster_directory + '/' + image_title)
+            labels[image_title] = temp_genres
         except OSError:
             print('Image not found')
     ordered_genres = list(genres)
     ordered_genres.sort()
-    with open('assets/genres.json', 'w') as outfile:
+    with open('genres.json', 'w') as outfile:
         json.dump(ordered_genres, outfile)
-    with open('assets/dataset.json', 'w') as outfile:
-        json.dump(dataset, outfile)
+    with open('labels.json', 'w') as outfile:
+        json.dump(labels, outfile)
 
 
 if __name__ == "__main__":
-    create_dataset('assets/imdb_output.json', '', 'assets/posters')
+    create_dataset('imdb_output.json', '', 'assets/posters')
