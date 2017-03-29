d10c6bc48fcb00158b2a4c699c0dc33356f74180
Guillaume <guillaume.guerdoux@hotmail.fr>
2017-03-29 09:31:58 +0200

    Add a validation set

--- train_val_test_set_creation.py
+++ train_val_test_set_creation.py
@@ -9,18 +9,25 @@ import pickle
     - test_set : name of tests movies
 '''
 training_set = set()
+validation_set = set()
 test_set = set()
 
 absolute_path = 'assets/posters/'
 for image in os.listdir(absolute_path):
-    train_prob = random.random()
-    if train_prob >= 0.2:
+    prob = random.random()
+    if prob >= 0.35:
         training_set.add(image)
+    elif prob >= 0.15:
+        validation_set.add(image)
     else:
         test_set.add(image)
 
+
 with open('training_set_list.pickle', 'wb') as handle:
     pickle.dump(training_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('validation_set_list.pickle', 'wb') as handle:
+    pickle.dump(validation_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 with open('test_set_list.pickle', 'wb') as handle:
     pickle.dump(test_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
